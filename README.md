dd6c40234f78151ae193892e1e6dc9dac153177d
Alexey Gumirov <ag_devops@die-optimisten.net>
2023-04-28 00:25:00 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # telegram-bot-chatgpt
+
 My telegram bot for ChatGPT (written with the help of ChatGPT)
+Bot is written in Python and is tested with the Python 3.10. 
+
+## Preparations
+
+1. Create Telegram bot and get bot token:
+    - Bot is created using **@BotFather** in Telegram.
+2. In your ChatGPT account (https://chat.openai.com) create your API key.
+
+## Parameters
+
+- TELEGRAM_API_TOKEN: your Telegram bot token. This parameter must be provided.
+- CHATGPT_API_KEY: your ChatGPT API key. This parameter must be provided.
+- GPT_MODEL: Which GPT model is going to be used for completion. Default model is "gpt-3.5-turbo".
+    - To get list of models look into the documentation: [OpenAI documentation](https://platform.openai.com/docs/api-reference/models/list)
+- ALLOWED_CHAT_IDS=<comma separated list of allowed account IDs>
+    - When no parameter is defined, then bot operated as public.
+    - When list of account IDs is given, bot communicates only with them.
+    - To get ID of Telegram account, use **@userinfobot**: forward to it message from the account you want to get ID from.
+- CHAT_HISTORY_SIZE: default value is 20 - it means that chat history will keep 10 messages of user and 10 answers from ChatGPT. You can setup your
+- MAX_TOKENS: The maximum number of tokens to generate in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length. Default value is 500. Maximum possible value is 4096. If you try to set value higher, program will set it to 4096.
+
+## Building and running
+
+### Requirements
+
+- Docker
+- Docker-compose
+
+### Instructions
+
+1. Clone repository
+2. Enter into the `src` directory.
+3. Copy `docker-compose-template.yaml` to `docker-compose.yaml` in this directory.
+4. In the `docker-compose.yaml` file set values for: TELEGRAM_API_TOKEN and CHATGPT_API_KEY. If you want private bot, also provide list of comma separated account IDs.
+5. Check that your docker-compose file is correct: `docker-compose config`.
+6. If no errors are found, run in the same directory: `docker-compose up -d`.
---
+++ src/app/telegram_bot.py
@@ -0,0 +1,163 @@
+import os
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+# from aiogram.types import ParseMode
+from aiogram.utils import executor
+import aiohttp
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+CHATGPT_API_KEY = os.getenv("CHATGPT_API_KEY")
+GPT_MODEL = os.getenv("GPT_MODEL", "gpt-3.5-turbo")
+MAX_TOKENS = min(int(os.getenv("MAX_TOKENS", 500)), 4096)
+
+# If comma separated list of chat IDs is provided, the bot will only work in those chats
+# If not provided, the bot will work in all chats
+if os.getenv("ALLOWED_CHAT_IDS"):
+    ALLOWED_CHAT_IDS = [int(chat_id) for chat_id in os.getenv("ALLOWED_CHAT_IDS").split(",")]
+    IS_PUBLIC = False
+else:
+    ALLOWED_CHAT_IDS = []
+    IS_PUBLIC = True
+
+if os.getenv("CHAT_HISTORY_SIZE"):
+    CHAT_HISTORY_SIZE = int(os.getenv("CHAT_HISTORY_SIZE"))
+else:
+    CHAT_HISTORY_SIZE = 20
+
+chat_history = {}
+# Create dictionary to keep track of chat IDs and messages history
+
+
+def add_message(chat_id, message):
+    if chat_id not in chat_history:
+        chat_history[chat_id] = []
+    chat_history[chat_id].append(message)
+    if len(chat_history[chat_id]) > CHAT_HISTORY_SIZE:
+        chat_history[chat_id].pop(0)
+
+
+def clean_history(chat_id):
+    chat_history[chat_id] = []
+
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=TELEGRAM_API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+
+async def is_allowed(user_id: int) -> bool:
+    if user_id in ALLOWED_CHAT_IDS:
+        return True
+    return IS_PUBLIC
+
+# Command handlers
+
+
+async def on_start(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    await message.answer(f"Hello! I am a ChatGPT bot.\nI am using {GPT_MODEL}.\nType your message and I'll respond.")
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    await message.reply(f"Hello! I'm a ChatGPT bot.\nI am using {GPT_MODEL}.\nSend me a message or a command, and I'll respond!")
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    help_text = (
+        "Here's a list of available commands:\n"
+        "/start - Start the bot\n"
+        "/help - Show this help message\n"
+        "/info - Get information about the bot\n"
+        "/status - Check the bot's status\n"
+        "/new_topic - Clear ChatGPT conversation history"
+    )
+    await message.reply(help_text)
+
+
+@dp.message_handler(commands=['info'])
+async def info_command(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    info_text = f"I'm a ChatGPT bot.\nI am using {GPT_MODEL}.\nI can respond to your messages and a few basic commands."
+    await message.reply(info_text)
+
+
+@dp.message_handler(commands=['status'])
+async def status_command(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    status_text = "I'm currently up and running!"
+    await message.reply(status_text)
+
+
+@dp.message_handler(commands=['new_topic'])
+async def status_command(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    clean_history(message.chat.id)
+    status_text = "ChatGPT conversation history is cleared!"
+    await message.reply(status_text)
+
+
+@dp.message_handler(content_types=types.ContentTypes.NEW_CHAT_MEMBERS)
+async def new_chat_member_handler(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    new_members = message.new_chat_members
+    me = await bot.get_me()
+
+    if me in new_members:
+        await on_start(message)
+        await help_command(message)
+
+# Message handlers
+
+
+@dp.message_handler()
+async def reply(message: types.Message):
+    if not await is_allowed(message.from_user.id):
+        return  # Ignore the message if the user is not allowed
+    input_text = message.text
+    add_message(message.chat.id, {"role": "user", "content": input_text})
+    response_text = await chatgpt_request(chat_history[message.chat.id])
+    await message.reply(response_text)
+    add_message(message.chat.id, {"role": "assistant", "content": response_text})
+
+
+async def chatgpt_request(messages_history):
+    headers = {
+        "Authorization": f"Bearer {CHATGPT_API_KEY}",
+    }
+
+    data = {
+        "model": "gpt-3.5-turbo",
+        "temperature": 0.7,
+        "top_p": 0.9,
+        "max_tokens": MAX_TOKENS,
+        "messages": messages_history
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data) as response:  # Change the URL to use gpt-3.5-turbo
+            response.raise_for_status()
+            result = await response.json()
+    return result["choices"][0]["message"]["content"].strip()
+
+if __name__ == "__main__":
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
